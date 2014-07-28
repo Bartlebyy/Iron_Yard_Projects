9e7afb28dacbe72b0501e3e1f56045043975ba5d
Diego Medina <diego@Diegos-MacBook-Air.local>
2014-07-28 13:23:10 -0400

    initial commit

---
+++ Day1HW.rb
@@ -0,0 +1,4 @@
+def hypotenuse(side_a, side_b)
+  side_c=((side_a**2)+(side_b**2))**(1/2)
+  puts "If Side A and B are equal to #{side_a} and #{side_b} inches respectively, then Side C is equal to #{side_c}."
+end
